2fcbad6ee648adceee62d2727a196d1430eb7976
foash-111 <mr2221@fayoum.edu.eg>
2023-10-15 16:37:32 +0300

    a function that prints num in binary shape

--- main.h
+++ main.h
@@ -17,12 +17,13 @@ typedef struct operator
 	int (*fun)(va_list);
 } op_t;
 
+int _pow_recursion(int x, int y);
 int print_binary(va_list pa);
-char *print_inreger(va_list pa);
-char *print_decimal(va_list pa);
 int print_string(va_list pa);
 int print_percentage(va_list pa);
 int print_char(va_list pa);
+char *print_inreger(va_list pa);
+char *print_decimal(va_list pa);
 void rev_string(char *s);
 int _strlen(char *s);
 int _printf(const char *format, ...);
