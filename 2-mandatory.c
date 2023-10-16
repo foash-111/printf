5017306b76e70ee1bee9385437225abcf26fc366
abdallahgamal110 <abdallahgamal840@gmail.com>
2023-10-16 22:30:27 +0300

    Update

---
+++ 2-mandatory.c
@@ -0,0 +1,41 @@
+#include"main.h"
+
+int print_integer(va_list pa)
+{
+	int len = 0, temp = 0 , i = 0, j = 0;
+	int number = va_arg(pa, int);
+char *ptr;
+	if (number < 0)
+	{
+		_putchar('-');
+		number = -number;
+	}
+	temp = number;
+	while (number)
+	{
+		number /= 10;
+		len++;
+	}
+
+	ptr = malloc((sizeof(char) * len) + 1 );
+
+	if(ptr == NULL)
+	return (0);
+
+while(i < len)
+{
+	ptr[i] = (temp % 10) + '0';
+	temp = temp / 10;
+	i++;
+}
+ptr[i] = '\0';
+i--;
+while (j < len)
+{
+	_putchar(ptr[i]);
+	i--;
+	j++;
+}
+free(ptr);
+return (len);
+}
