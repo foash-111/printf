d2723323e7bd7dc5cee367897bc97e1efc38c215
abdallahgamal110 <abdallahgamal840@gmail.com>
2023-10-17 20:53:19 +0300

    A

--- 2-mandatory.c
+++ 2-mandatory.c
@@ -6,9 +6,9 @@
 */
 int print_integer(va_list pa)
 {
-	int len = 0, temp = 0 , i = 0, j = 0;
+	int len = 0, temp = 0, i = 0, j = 0;
 	int number = va_arg(pa, int);
-char *ptr;
+	char *ptr;
 	if (number < 0)
 	{
 		_putchar('-');
@@ -20,11 +20,11 @@ char *ptr;
 		number /= 10;
 		len++;
 	}
-	ptr = malloc((sizeof(char) * len) + 1 );
+	ptr = malloc((sizeof(char) * len) + 1);
 
-	if(ptr == NULL)
+	if (ptr == NULL)
 	return (0);
-while(i < len)
+while (i < len)
 {
 	ptr[i] = (temp % 10) + '0';
 	temp = temp / 10;
@@ -49,6 +49,34 @@ return (len);
 */
 int print_INT_MAX(va_list pa)
 {
+    int len = 0, temp = 0, i = 0, j = 0;
     int number = va_arg(pa, int);
-    
+    char *ptr;
+
+    temp = number;
+    while (number)
+    {
+        number /= 10;
+        len++;
+    }
+    ptr = malloc((sizeof(char) * len) + 1);
+
+    if (ptr == NULL)
+        return (0);
+    while (i < len)
+    {
+        ptr[i] = (temp % 10) + '0';
+        temp = temp / 10;
+        i++;
+    }
+    ptr[i] = '\0';
+    i--;
+    while (j < len)
+    {
+        _putchar(ptr[i]);
+        i--;
+        j++;
+    }
+    free(ptr);
+    return (len);
 }
